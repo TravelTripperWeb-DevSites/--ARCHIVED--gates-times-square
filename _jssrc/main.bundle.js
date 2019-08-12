fdbc29967dea910b794c0c9f66175e87bebebf45
Raghuveer <rpanchagnula@traveltripper.com>
2019-08-12 17:40:31 +0530

    Home page banner slider in progress

--- _jssrc/main.bundle.js
+++ _jssrc/main.bundle.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+
+//fallback ready document
+function readyDoc(fn) {
+  var d = document;
+  d.readyState == 'loading' ? d.addEventListener('DOMContentLoaded', fn) : fn();
+}
+
+readyDoc(function () {
+  // Home page hero carousel
+  if (document.getElementsByClassName("home-hero-slider")[0]) {
+    var offersSlider = tns({
+      container: '.home-hero-slider',
+      "items": 1.5,
+      "slideBy": 1.5,
+      "mouseDrag": true,
+      "swipeAngle": false,
+      "speed": 400,
+      "nav": false
+    });
+  }
+});
