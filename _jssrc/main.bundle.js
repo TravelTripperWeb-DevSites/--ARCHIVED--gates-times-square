243545b0ebd3e206f666238f1ec2e558741bb3ae
Srinivas <rsrinivas13@gmail.com>
2019-08-12 18:38:39 +0530

    Top banner added

--- _jssrc/main.bundle.js
+++ _jssrc/main.bundle.js
@@ -1,4 +1,35 @@
-'use strict';
+"use strict";
+
+// To get the cookie value which is already set. If not set, it returns null
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+    begin = dc.indexOf(prefix);
+    if (begin != 0) return null;
+  } else {
+    begin += 2;
+    var end = document.cookie.indexOf(";", begin);
+    if (end == -1) {
+      end = dc.length;
+    }
+  }
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+// To set a cookie and it's expiry time in days
+function setCookie(name, value, days) {
+  var date, expires;
+  if (days) {
+    date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toGMTString();
+  } else {
+    expires = "";
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
 
 //fallback ready document
 function readyDoc(fn) {
@@ -19,4 +50,17 @@ readyDoc(function () {
       "nav": false
     });
   }
+
+  // If we click on close button, the cookie exclusive-offer-viewed sets to "yes" with an expiry date 1 day.
+
+  var offerCookie = getCookie("exclusive-offer-viewed");
+  if (offerCookie == null) {
+    // console.log("exclusive-offer-not-viewed");
+    document.getElementsByClassName('top-banner')[0].classList.remove("hide");
+  }
+
+  document.getElementsByClassName("top-banner__close-btn")[0].onclick = function () {
+    document.getElementsByClassName('top-banner')[0].classList.add("hide");
+    setCookie("exclusive-offer-viewed", "yes", 1);
+  };
 });
