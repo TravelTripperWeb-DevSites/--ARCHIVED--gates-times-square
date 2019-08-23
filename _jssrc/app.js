0d80e20fb6bd8b9a0f7724219e6ff4bccdb35e79
Raghuveer <rpanchagnula@traveltripper.com>
2019-08-23 15:14:29 +0530

    regionblocks added to home page

--- _jssrc/app.js
+++ _jssrc/app.js
@@ -8,19 +8,25 @@ readyDoc(function() {
 
 
   var weatherKey = 'b03f57fc9998146fc56895a9b59d1661';
-  fetch('https://crossorigin.me/https://api.darksky.net/forecast/' + weatherKey + '/24.5710009,-81.7566775?units=us')
-    .then(function(localWeather) {
-      var temF = localWeather.currently.temperature;
-      var temC = (temF - 32) * (5 / 9);
-      var html = '<p>Current Weather<br>Times Square, Florida</p><div class="visualtour__weather__icon"><i class="wi ' + localWeather.hourly.icon + '"></i></div><div class="visualtour__weather__temp">' + Math.round(temF) + '&deg;F ' + Math.round(temC) + '&#8451; <br> <a href="https://darksky.net/forecast/24.5711,-81.7531/us12/en" aria-hidden="true" rel="nofollow" target="_blank">Powered by Dark Sky</a></div>';
-      document.getElementById("weather").innerHTML = html;
-    })
-    .then(function(localWeather) {
-      var temF = localWeather.currently.temperature;
-      var temC = (temF - 32) * (5 / 9);
-      var html = '<p>Current Weather<br>Times Square, Florida</p><div class="visualtour__weather__icon"><i class="wi ' + localWeather.hourly.icon + '"></i></div><div class="visualtour__weather__temp">' + Math.round(temF) + '&deg;F ' + Math.round(temC) + '&#8451; <br> <a href="https://darksky.net/forecast/24.5711,-81.7531/us12/en" aria-hidden="true" rel="nofollow" target="_blank">Powered by Dark Sky</a></div>';
-      document.getElementById("weather").innerHTML = html;
-    });
+
+  axios({
+    method: 'get',
+    url: 'https://cors.io/?https://api.darksky.net/forecast/' + weatherKey + '/24.5710009,-81.7566775?units=us',
+    dataType: 'jsonp'
+  })
+  .then(function (localWeather) {
+    var temF = localWeather.currently.temperature;
+    var temC = (temF - 32) * (5 / 9);
+    var html = '<p>Current Weather<br>Times Square, Florida</p><div class="visualtour__weather__icon"><i class="wi ' + localWeather.hourly.icon + '"></i></div><div class="visualtour__weather__temp">' + Math.round(temF) + '&deg;F ' + Math.round(temC) + '&#8451; <br> <a href="https://darksky.net/forecast/24.5711,-81.7531/us12/en" aria-hidden="true" rel="nofollow" target="_blank">Powered by Dark Sky</a></div>';
+    document.getElementById("weather").innerHTML = html;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 
 
   // Home page hero carousel
